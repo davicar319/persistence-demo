d1de0495fa4463152c50e64b8b3523457062952b
Dave Carlson <dave.carlson@tr.com>
2019-11-22 14:22:23 -0600

    Adding jpa annotations to officer

--- src/main/java/com/bixbysdoghouse/bootfunamentals/module3/persistencedemo/entities/Officer.java
+++ src/main/java/com/bixbysdoghouse/bootfunamentals/module3/persistencedemo/entities/Officer.java
@@ -1,16 +1,31 @@
 package com.bixbysdoghouse.bootfunamentals.module3.persistencedemo.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
+@Entity
+@Table(name = "officers")
 @Data
 @AllArgsConstructor
 public class Officer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Enumerated(EnumType.STRING)
     private Rank rank;
+    @Column(name="first_name", nullable = false)
     private String first;
+    @Column(name = "last_name", nullable = false)
     private String last;
 
     public Officer(@NonNull Rank rank, @NonNull String first, @NonNull String last) {
